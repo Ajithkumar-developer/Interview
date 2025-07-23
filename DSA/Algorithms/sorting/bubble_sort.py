91ba4307c982b2731c73dda1ee78cc5632eba797
Ajith kumar <ajith47csr@gmail.com>
2025-07-23 11:17:53 +0530

    Updating bubble sort

--- DSA/Algorithms/sorting/bubble_sort.py
+++ DSA/Algorithms/sorting/bubble_sort.py
@@ -1,1 +1,14 @@
 # bubble sort
+def bubble_sort(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+
+
+
+# Example
+nums = [5, 2, 9, 1, 5, 6]
+bubble_sort(nums)
+print("Sorted array:", nums)

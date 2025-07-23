fad09a6ff36f621fa31eedc5726c970593fdb604
Ajith kumar <ajith47csr@gmail.com>
2025-07-23 17:29:50 +0530

    Updating Binary Search

--- DSA/Algorithms/searching/binary_search.py
+++ DSA/Algorithms/searching/binary_search.py
@@ -3,17 +3,19 @@
 def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
+    result = -1
 
     while low <= high:
         mid = (low + high) // 2
         if arr[mid] == target:
-            return mid  # Found
+            result = mid
+            high = mid - 1
         elif arr[mid] < target:
             low = mid + 1
         else:
             high = mid - 1
 
-    return -1  # Not found
+    return result
 
 numbers = [1, 3, 5, 7, 9, 11, 13]
 target = 7
